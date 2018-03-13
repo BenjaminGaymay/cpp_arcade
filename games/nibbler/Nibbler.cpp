ae1d138084e1c88c92da411680f883168661ef3a
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-03-13 12:50:52 +0100

    nibbler update

--- games/nibbler/Nibbler.cpp
+++ games/nibbler/Nibbler.cpp
@@ -24,8 +24,6 @@ void arcade::Nibbler::initMap()
 
 	_map.clear();
 	_nibblerPos.clear();
-	_applePos.clear();
-	_wallPos.clear();
 
 	_map.push_back("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
 	_map.push_back("W  R         R   R         R  W");
@@ -42,7 +40,7 @@ void arcade::Nibbler::initMap()
 	_map.push_back("W WWWWWWWW WWWWWWWWW WWWWWWWW W");
 	_map.push_back("WR     R               R     RW");
 	_map.push_back("WWWWW WWWWWWWWWRWWWWWWWWW WWWWW");
-	_map.push_back("WR     R GGGG          R     RW");
+	_map.push_back("WR     R               R     RW");
 	_map.push_back("W WWWWWWWWWWWWW WWWWWWWWWWWWW W");
 	_map.push_back("W  R          W W          R  W");
 	_map.push_back("W WWWWWWWWWWW  R  WWWWWWWWWWW W");
@@ -59,71 +57,40 @@ void arcade::Nibbler::initMap()
 	_map.push_back("WR        R         R        RW");
 	_map.push_back("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
 
-	for (unsigned line = 0 ; line < _map.size()  ; line++) {
-		for (int column = _map[line].size() - 1 ; column >= 0 ; column--) {
-			switch (_map[line][column]) {
-				case 'R':
-					_applePos.push_back({line, column}); _map[line][column] = ' '; break;
-				case 'G':
-					_nibblerPos.push_back({line, column}); _map[line][column] = ' '; break;
-				case 'W':
-					_wallPos.push_back({line, column}); _map[line][column] = ' '; break;
-			}
-		}
-	}
-
+	_nibblerPos.push_back({15, 12});
+	_nibblerPos.push_back({15, 11});
+	_nibblerPos.push_back({15, 10});
+	_nibblerPos.push_back({15, 9});
 	_pause = false;
 }
 
 void arcade::Nibbler::fillMap()
 {
-	for (auto &pos : _applePos)
-		_map[pos.first][pos.second] = 'R';
-
 	for (auto &pos : _nibblerPos)
 		_map[pos.first][pos.second] = 'G';
-
-	for (auto &pos : _wallPos)
-		_map[pos.first][pos.second] = 'W';
 }
 
 void arcade::Nibbler::clearMap()
 {
-	for (auto &pos : _applePos)
-		_map[pos.first][pos.second] = ' ';
-
 	for (auto &pos : _nibblerPos)
 		_map[pos.first][pos.second] = ' ';
 }
-
-// void arcade::Nibbler::addApple()
-// {
-// 	static auto size = _map.size() - 2;
-
-// 	_applePos.push_back({rand() % size + 1, rand() % (size - 2) + 1});
-// }
 
 void arcade::Nibbler::getNewSide()
 {
 	switch (_key) {
 		case UP:
-			_x = 0; _y = -1;
-			break;
+			_x = 0; _y = -1; break;
 		case LEFT:
-			_y = 0;	_x = -1;
-			break;
+			_y = 0;	_x = -1; break;
 		case DOWN:
-			_x = 0; _y = 1;
-			break;
+			_x = 0; _y = 1; break;
 		case RIGHT:
-			_y = 0;	_x = 1;
-			break;
+			_y = 0;	_x = 1; break;
 		case PAUSE:
-			_pause = _pause ? false : true;
-			break;
+			_pause = _pause ? false : true; break;
 		case ESC:
-			initMap();
-			break;
+			initMap(); break;
 		default:
 			break;
 	}
@@ -131,22 +98,17 @@ void arcade::Nibbler::getNewSide()
 
 void arcade::Nibbler::checkCollision(std::pair<std::size_t, std::size_t> &pos)
 {
-	auto wallCollision = std::find(_wallPos.begin() + 1, _wallPos.end(), pos);
-	auto nibblerCollision = std::find(_nibblerPos.begin() + 1, _nibblerPos.end(), pos);
+	auto nibbler = find(_nibblerPos.begin() + 1, _nibblerPos.end(), pos);
 
-	if (nibblerCollision != _nibblerPos.end() or wallCollision != _wallPos.end())
+	if (_map[pos.first][pos.second] == 'W' or nibbler != _nibblerPos.end())
 		initMap();
 }
 
 bool arcade::Nibbler::checkApple(std::pair<std::size_t, std::size_t> &pos)
 {
-	auto apple = std::find(_applePos.begin(), _applePos.end(), pos);
-
-	if (apple != _applePos.end()) {
-		_applePos.erase(apple);
-		//addApple();
+	if (_map[pos.first][pos.second] == 'R')
 		return false;
-	}
+
 	return true;
 }
 
@@ -155,16 +117,7 @@ void arcade::Nibbler::moveNibbler()
 	std::pair<std::size_t, std::size_t> pos;
 
 	pos.first = _nibblerPos[0].first + _y;
-	// if (pos.first >= _map.size() - 1)
-	// 	pos.first = 1;
-	// else if (pos.first < 1)
-	// 	pos.first = _map.size() - 2;
-
 	pos.second = _nibblerPos[0].second + _x;
-	// if (pos.second >= _map.size() - 3)
-	// 	pos.second = 1;
-	// else if (pos.second < 1)
-	// 	pos.second = _map.size() - 4;
 
 	_nibblerPos.insert(_nibblerPos.begin(), pos);
 
@@ -182,7 +135,7 @@ bool arcade::Nibbler::doLoop()
 {
 	auto period = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now() - _previousLoop).count();
 
-	if (period < 300)
+	if (period < 150)
 		return false;
 
 	_previousLoop = std::chrono::system_clock::now();
@@ -217,7 +170,6 @@ void arcade::Nibbler::showMap(std::unique_ptr<IGraphics> &lib)
 
 void arcade::Nibbler::start(std::unique_ptr<arcade::IGraphics> &lib)
 {
-	lib->clearWindow();
 	fillMap();
 	showMap(lib);
 	clearMap();
